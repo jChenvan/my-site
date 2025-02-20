358c092d887e3f65b52901c9cde447832e7d51ba
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2025-02-20 15:35:28 -0500

    Add scroll/resize handling

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import scrollAnimation from './scrollAnimation'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let lastScrollPos;
+const scrollFactor = 0.002;
 
-setupCounter(document.querySelector('#counter'))
+window.onscroll = () => {
+    if (lastScrollPos) {
+        scrollAnimation.update((window.scrollY - lastScrollPos)*scrollFactor);
+    }
+    lastScrollPos = window.scrollY;
+}
+
+const containter = document.querySelector('.scroll-animation');
+
+window.onresize = () => {
+    scrollAnimation.resize(containter.clientWidth,containter.clientHeight);
+}
